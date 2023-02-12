2df443ce9c1c9f31c795509543bc529daf8bdcd0
MateDawid <mateusiakdawid@gmail.com>
2023-02-12 22:11:21 +0100

    API connector methods to get API data as table

--- sw_explorer/characters/utils.py
+++ sw_explorer/characters/utils.py
@@ -14,3 +14,22 @@ class SWAPIConnector:
         if response.ok:
             return response.json()
         return {"status_code": response.status_code, 'text': response.text}
+
+    def get_all_pages(self, endpoint=''):
+        data = []
+        response = self.get(endpoint)
+        data += response.get('results', [])
+        next_link = response.get('next')
+        while next_link is not None:
+            response = self.get(next_link)
+            data += response.get('results')
+            next_link = response.get('next')
+        return data
+
+    def get_as_table(self, endpoint=''):
+        data = self.get_all_pages(endpoint)
+        if not data:
+            return data
+        headers = [header for header in data[0].keys()]
+        rows = [[value for value in person.values()] for person in data]
+        return [headers] + rows
